e0117436fdc37bf8b9c193e9eb8b42c938cc055b
Gustavo Adolfo López Delgado <hust_mw2000@hotmail.com>
2025-02-07 10:13:22 -0600

    Proyecto Sprint

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface extra {
+  nombre: string;
+  precio: number;
+}
+
 @Component({
   selector: 'app-main',
   imports: [CommonModule],
@@ -30,5 +35,49 @@ export class MainComponent {
 
   selectImage(image: string) {
     this.selectedImage = image;
+  }
+  contador: number = 0;
+  contador2: number = 0;
+  total: number = 0;
+
+  aumentar() {
+    this.contador++;
+    this.total = this.contador*500;
+  }
+
+  disminuir() {
+    
+    if (this.contador > 0){
+      this.contador--;
+      this.total = this.total-500;
+    }
+  }
+  aumentar2() {
+    this.contador2++;
+  }
+
+  disminuir2() {
+    
+    if (this.contador > 0){
+      this.contador2--;
+    } 
+  }
+
+  extras: extra [] = [
+    {nombre: 'mascota', precio: 250},
+    {nombre: 'mascota', precio: 250},
+    {nombre: 'mascota', precio: 250},
+    {nombre: 'mascota', precio: 250},
+    {nombre: 'mascota', precio: 250}
+  ];
+
+
+  actualizarTotal(extra: extra, event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      this.total += extra.precio;
+    } else {
+      this.total -= extra.precio;
+    }
   }
 }
